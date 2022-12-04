33cb2ed751c07fb5e50060ec43de111a4634504f
notsamzhou <notsamzhou@gmail.com>
2022-12-04 13:21:37 +0000

    fixes

--- src/clean.py
+++ src/clean.py
@@ -6,13 +6,13 @@ def clean():
         os.system("rm .png")
                       
     if os.path.exists(os.getcwd() + '/data/out'):
-        if os.listdir('/data/out'):
+        if os.listdir('/data/out/'):
             os.system("rm data/out/*")
         
     if os.path.exists(os.getcwd() + '/data/temp'):
-        if os.listdir('/data/temp'):
+        if os.listdir('/data/temp/'):
             os.system("rm data/temp/*")
     
     if os.path.exists(os.getcwd() + '/test/out'):
-        if os.listdir('/test/out'):
+        if os.listdir('/test/out/'):
             os.system("rm test/out/*")
